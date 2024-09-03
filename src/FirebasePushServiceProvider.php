b88de7e96b558cda5ee92da681d9249a45320ec7
shijazi <mohamadsafwan.hijazi@gmail.com>
2024-09-03 10:12:33 +0300

    add some enhancement

--- src/FirebasePushServiceProvider.php
+++ src/FirebasePushServiceProvider.php
@@ -8,16 +8,12 @@ class FirebasePushServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(__DIR__.'/../config/firebase_push.php', 'firebase_push');
-
-        $this->app->singleton(FirebasePushService::class, function ($app) {
-            return new FirebasePushService(config('firebase_push.server_key'));
-        });
     }
 
     public function boot()
     {
         $this->publishes([
             __DIR__.'/../config/firebase_push.php' => config_path('firebase_push.php'),
-        ]);
+        ], 'firebase-push-config');
     }
 }
