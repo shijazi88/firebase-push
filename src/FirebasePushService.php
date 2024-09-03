9fb5f3093058bc57b3e368f51632de07042c4afd
shijazi <mohamadsafwan.hijazi@gmail.com>
2024-09-03 10:41:20 +0300

    add some enhancement

--- src/FirebasePushService.php
+++ src/FirebasePushService.php
@@ -13,6 +13,7 @@ class FirebasePushService
         $this->serverKey = $serverKey;
     }
 
+    // Send notification to multiple devices with the same message
     public function sendNotification($title, $body, $tokens)
     {
         if (count($tokens) === 0) {
@@ -24,6 +25,7 @@ class FirebasePushService
         return $this->handleResponse($response);
     }
 
+    // Send different notifications to multiple devices
     public function sendToMultipleDevices(array $notifications)
     {
         $responses = [];
@@ -42,6 +44,50 @@ class FirebasePushService
         return $responses;
     }
 
+    // Send notification to a single device
+    public function sendToSingleDevice($title, $body, $token)
+    {
+        return $this->sendNotification($title, $body, [$token]);
+    }
+
+    // Send notification to a Firebase topic
+    public function sendToTopic($title, $body, $topic)
+    {
+        $url = 'https://fcm.googleapis.com/fcm/send';
+        $data = [
+            "to" => "/topics/" . $topic,
+            "notification" => [
+                "title" => $title,
+                "body" => $body,
+            ],
+        ];
+
+        $response = Http::withHeaders($this->prepareHeaders())
+            ->post($url, $data);
+
+        return $this->handleResponse($response);
+    }
+
+    // Send notification with custom data
+    public function sendWithCustomData($title, $body, $tokens, array $customData = [])
+    {
+        $url = 'https://fcm.googleapis.com/fcm/send';
+        $data = [
+            "registration_ids" => $tokens,
+            "notification" => [
+                "title" => $title,
+                "body" => $body,
+            ],
+            "data" => $customData,
+        ];
+
+        $response = Http::withHeaders($this->prepareHeaders())
+            ->post($url, $data);
+
+        return $this->handleResponse($response);
+    }
+
+    // Helper methods
     protected function makeRequest($title, $body, $tokens)
     {
         $url = 'https://fcm.googleapis.com/fcm/send';
